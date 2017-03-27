18f916c0455cbbcdd1478e35fa8dba2a9f6c3901
Chengyu Yao <chengyu2005@hotmail.com>
2017-03-27 13:55:25 -0700

    fix views

--- CatCafe/config/routes.rb
+++ CatCafe/config/routes.rb
@@ -4,9 +4,12 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get 'login' => 'login#login'
+  get 'sign_up' => 'login#sign_up'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
