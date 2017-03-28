a397e69fe7c93fb464bf9371bc3003d5c01842ac
Chengyu Yao <chengyu2005@hotmail.com>
2017-03-27 17:41:55 -0700

    member adopting cat

--- CatCafe/app/controllers/login_controller.rb
+++ CatCafe/app/controllers/login_controller.rb
@@ -1,5 +1,8 @@
 class LoginController < ApplicationController
   def login
+  	# if successful render the screen for member
+  	id = 0
+  	redirect_to controller: 'member', action: 'main', id: id
   end
   def sign_up
   end
--- CatCafe/app/controllers/member_controller.rb
+++ CatCafe/app/controllers/member_controller.rb
@@ -1,2 +1,16 @@
 class MemberController < ApplicationController
+	def main
+	end
+
+	def adopt
+		@available_cats = [["Meow", 1],["Roar", 2]]
+	end
+
+	def adopt_submit
+		#TODO Submit the adoption with params
+		@memberId = params[:id]
+		@age = params[:member][:age]
+		@phone = params[:member][:phone]
+		@catId = params[:cat_id]
+	end
 end
