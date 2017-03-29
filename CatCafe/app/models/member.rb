1478444216c16f3e98066e34a2dcbc5e9ca06ac7
Chengyu Yao <chengyu2005@hotmail.com>
2017-03-29 15:37:06 -0700

    member model

--- CatCafe/app/models/member.rb
+++ CatCafe/app/models/member.rb
@@ -1,20 +1,86 @@
 class Member < ApplicationRecord
 
 	# Find member with id = id
-	def find(id)
-		results = ActiveRecord::Base.connection.execute("SELECT * FROM members WHERE (members.memberId = " + id + ");" )
-		if results.present?
-			return results.first
-		else
-			return nil
-		end
+	def findById(id)
+		results = nil
+		sqlQuery = "SELECT * FROM Members WHERE (MemberID = " + id + ");"
+		begin  
+  			ActiveRecord::Base.transaction do
+				results = ActiveRecord::Base.connection.execute(sqlQuery)
+  			end
+ 		rescue Exception => exc
+   			return exc;
+ 		end
+ 		return results
+	end
+
+	# Find member with email
+	def findByEmail(Email)
+		results = nil
+		sqlQuery = "SELECT * FROM Members WHERE (Email = " + Email + ");"
+		begin  
+  			ActiveRecord::Base.transaction do
+				results = ActiveRecord::Base.connection.execute(sqlQuery)
+  			end
+ 		rescue Exception => exc
+   			return exc;
+ 		end
+ 		return results
 	end
 
 	#TODO create and edit should have their respective params
-	def create
+	def create(MName, PhoneNum, Email, MPassword)
+		results = nil
+		sqlQuery = "INSERT INTO Members (MName, PhoneNum, Email, MPassword) VALUE " 
+		sqlQuery = sqlQuery + "(" MName + "," + PhoneNum + "," + Email + "," + MPassword + ");"
+		begin
+  			ActiveRecord::Base.transaction do
+				result = ActiveRecord::Base.connection.execute(sqlQuery)
+  			end
+ 		rescue Exception => exc
+   			return exc;
+ 		end
+ 		return results
 	end
 
-	def edit
+	def edit(MemberID, MName, PhoneNum, Email, MPassword)
+		results = nil
+		sqlQuery = "UPDATE Members SET "
+		if (!MName.nil?)
+			sqlQuery = sqlQuery + "MName = \'" + MName + "\', "
+		end
+		if (!PhoneNum.nil?)
+			sqlQuery = sqlQuery + "PhoneNum = \'" + PhoneNum + "\', "
+		end
+		if (!Email.nil?)
+			sqlQuery = sqlQuery + "Email = \'" + Email + "\', "
+		end
+		if (!MPassword.nil?)
+			sqlQuery = sqlQuery + "MPassword = \'" + MPassword + "\' "
+		end
+		sqlQuery = sqlQuery + "WHERE MemberID = " + MemberID + ";"
+
+		begin  
+  			ActiveRecord::Base.transaction do
+				result = ActiveRecord::Base.connection.execute(sqlQuery)
+  			end
+ 		rescue Exception => exc
+   			return exc;
+ 		end
+ 		return results
 	end
 
+	def destroy(MemberID)
+		results = nil
+		sqlQuery = "DELETE FROM Members WHERE MemberID = " + MemberID + ";"
+
+		begin  
+  			ActiveRecord::Base.transaction do
+				result = ActiveRecord::Base.connection.execute(sqlQuery)
+  			end
+ 		rescue Exception => exc
+   			return exc;
+ 		end
+ 		return results
+ 	end
 end
