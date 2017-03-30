d298aebcf14a1dcd056c917f7dc8ef28290b9753
Ana Lu <anangoclu@gmail.com>
2017-03-29 21:57:37 -0700

    employee controller and some work on login view

--- CatCafe/app/controllers/employee_controller.rb
+++ CatCafe/app/controllers/employee_controller.rb
@@ -1,2 +1,73 @@
 class EmployeeController < ApplicationController
+	def main
+	end
+
+	def find_employee
+		@employeeId = params[:employee][:id]
+		result = Employee.find(@employeeId)
+		@employee = result.first
+	end
+
+	def create_employee
+		@ename = params[:employee][:name]
+		@password = password[:employee][:password]
+		Employee.create(@ename, @password)
+	end
+
+	def edit_employee
+		@employeeId = params[:employee][:id]
+		@ename = params[:employee][:name]
+		@password = password[:employee][:password]
+		Employee.edit(@employeeId, @ename, @password)
+	end
+
+	def isManager
+		@employeeId = params[:employee][:id]
+		isManager = Employee.isManager(@employeeId)
+	end
+
+	def find_transaction
+		@transactionId = params[:transaction][:id]
+		result = Transaction.find(@transactionId)
+	end
+
+	def create_transaction
+		@referenceId = params[:transaction][:refId]
+		@paymentType = params[:pay_type]
+		@trxnTotal = params[:transaction][:total]
+		@trxnDate = params[:transaction][:day]
+		@employeeId = params[:employee][:id]
+		#TODO: insert params
+		Transaction.create()
+	end
+
+	def edit_transaction
+		@trxnId =params[:transaction][:id]
+		@referenceId = params[:transaction][:refId]
+		@paymentType = params[:pay_type]
+		@trxnTotal = params[:transaction][:total]
+		@trxnDate = params[:transaction][:day]
+		@employeeId = params[:employee][:id]
+		#TODO: insert params
+		Transaction.edit()
+	end
+
+	def find_item
+		@itemId = params[:item][:id]
+		result = Item.find(@itemId)
+		@item = @itemId.first
+	end
+
+	def create_item
+		@name = params[:item][:name]
+		@price = params[:item][:price]
+		Item.create(@name, @price)
+	end
+
+	def edit_item
+		@itemId = params[:item][:id]
+		@name = params[:item][:name]
+		@price = params[:item][:price]
+		Item.edit(@itemId)
+	end
 end
